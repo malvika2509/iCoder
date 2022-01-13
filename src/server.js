372e01f26821afc372917e5817267228d90466f2
malvika2509 <71529259+malvika2509@users.noreply.github.com>
2022-01-14 01:54:48 +0530

    commit

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const cookieParser=require("cookie-parser");
 
 
 require("./db/connection");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 const static_path=path.join(__dirname,"../public");
 const template_path=path.join(__dirname,"/templates/views");
