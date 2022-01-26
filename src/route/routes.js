85e47aac07c226e79e054828b787086d57ddfc73
malvika2509 <71529259+malvika2509@users.noreply.github.com>
2022-01-26 14:18:46 +0530

    code changed

--- src/route/routes.js
+++ src/route/routes.js
@@ -138,7 +138,7 @@ router.post('/form.hbs', async (req, res) => {
             const registered = await registerStudent.save();
             console.log("The page part " + registered);
 
-            res.status(201).render("cover");
+            res.status(201).render("cover.hbs");
         } else {
             res.send("Passwords not match");
         }
